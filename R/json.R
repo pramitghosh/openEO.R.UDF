313428d0cafd982934de11cf92b5fd19de7db317
Pramit Ghosh <pramitghosh@outlook.com>
2018-08-08 18:18:25 +0200

    Changed plumber serializer for `run_UDF.json()`
    - Changed serializer to "unboxedJSON"
    - Changed template of user's UDF script so that function name is not needed in the JSON anymore
    - Added calls to Sys.time() to monitor time elapsed in important segments
    - Output JSON now corresponds to specs (no "code")
    - Uploaded sample JSON request and response and a sample UDF script for users

--- R/json.R
+++ R/json.R
@@ -2,9 +2,12 @@ json2script = function(json)
 {
   lang = json$code$language
   script = json$code$source
-  sink(paste("tmp_udf", "R", sep = "."))
+  # Todo: Source the string `script` directly without writing it to disk
+  # Note: Need to remove escape characters in `script`
+  sink(file = "tmp_udf.R")
   cat(script)
   sink()
+  print(Sys.time())
   cat("Extracted script for JSON!\n")
 }
 
@@ -47,6 +50,7 @@ tile2raster = function(tile, time_num, proj)
 
 json2stars = function(json)
 {
+  print(Sys.time())
   cat("Started converting JSON to stars...\n")
   proj_string = json$data$proj
   num_bands = length(json$data$raster_collection_tiles)
@@ -59,18 +63,23 @@ json2stars = function(json)
 
   timestamps = strptime(json$data$raster_collection_tiles[[1]]$start_times, format = "%Y-%m-%dT%T", tz = "Europe/Berlin")
   timestamps_padded = c(timestamps, timestamps[length(timestamps)]+diff(timestamps)[1]) #Need to start an issue in `stars`
+
+  #Todo: Get rid of the nested for loops below and in `tile2raster()`
+  #Todo: Think about using dataframes/martices
   for(band_num in 1:num_bands)
   {
-    cat(paste("Processing Band: ", band_num, "; ", sep = ""))
+    cat(paste(Sys.time(), "; Processing Band: ", band_num, "; ", sep = ""))
     tile = json$data$raster_collection_tiles[[band_num]]
     for(time_num in 1:num_time)
     {
-      cat(paste("Time: ", time_num, "...\n", sep = ""))
+      cat(paste(Sys.time(), "; Time: ", time_num, "...\n", sep = ""))
       bt_list[[time_num]] = c(bt_list[[time_num]], tile2raster(tile, time_num, proj_string))
     }
   }
-  cat("Finished converting JSON to Raster objects!")
+  print(Sys.time())
+  cat("Finished converting JSON to Raster objects!\n")
   stars_obj = NULL
+  print(Sys.time())
   cat("Starting to convert Raster to stars objects...\n")
   for(time_num in 1:num_time)
   {
@@ -97,12 +106,14 @@ json2stars = function(json)
       attr(stars_obj, "dimensions")[["time"]]$delta = mean(diff(timestamps))
     }
   }
+  print(Sys.time())
   cat("Converted JSON to stars object!\n")
   stars_obj
 }
 
-run_script = function(stars_obj, dim_mod, function_name, script_file = "tmp_udf.R")
+run_script = function(stars_obj, dim_mod, script_file = "tmp_udf.R")
 {
+  print(Sys.time())
   cat("Started executing UDF on stars object...\n")
   # dim_mod = 1, 2 means space
   # dim_mod = 3 means band
@@ -123,30 +134,32 @@ run_script = function(stars_obj, dim_mod, function_name, script_file = "tmp_udf.
 
   if(file.exists(script_file))
   {
-    source(script_file)
+    function_name = source(script_file)$value
     result = st_apply(stars_obj, FUN = function_name, MARGIN = all_dim[-c(dim_mod)])
     new_dim = all_dim
     new_dim[dim_mod] = NA
   } else
     stop("Script file is unavailable!")
+  print(Sys.time())
   cat("Applied UDF on stars object!\n")
   result
 }
 
 stars2json = function(stars_obj, json_in)#, json_out_file = "udf_response.json")
 {
+  print(Sys.time())
   cat("Started converting stars object to JSON...\n")
-  json_out = json_in #Copying structure of JSON
-  json_out$code = list()
-  json_out$data$proj = attr(stars_obj, "dimensions")$x$refsys
+  json_out = json_in[["data"]] #Copying structure of JSON but only the element "data"
+  # json_out$code = list()
+  json_out$proj = attr(stars_obj, "dimensions")$x$refsys
   tot_bands = as.numeric(dim(stars_obj)["band"])
   if(!is.na(tot_bands))
   {
-    json_out$data$raster_collection_tiles = json_out$data$raster_collection_tiles[-c(tot_bands+1:length(json_out$data$raster_collection_tiles))]
+    json_out$raster_collection_tiles = json_out$raster_collection_tiles[-c(tot_bands+1:length(json_out$raster_collection_tiles))]
     for(bands in 1:tot_bands)
     {
-      cat(paste("Band: ", bands, "; ", sep = ""))
-      tmp_extent = json_out$data$raster_collection_tiles[[bands]][["extent"]]
+      cat(paste(Sys.time(), "; Band: ", bands, "; ", sep = ""))
+      tmp_extent = json_out$raster_collection_tiles[[bands]][["extent"]]
       # Need another robust way to loop over bands & time since using `attr()` in the manner
       # below will not work for stars objects with arbitrary dimensions
       if("time" %in% dimnames(stars_obj))
@@ -167,26 +180,27 @@ stars2json = function(stars_obj, json_in)#, json_out_file = "udf_response.json")
         y2 = attr(stars_obj[,,,bands], "dimensions")$y$offset + delta_y * attr(stars_obj[,,,bands], "dimensions")$y$to
       }
       tmp_extent = list(north = max(y1,y2), south = min(y1,y2), west = min(x1,x2), east = max(x1,x2), height = if(sign(delta_y) < 0) -1 * delta_y else delta_y, width = if(sign(delta_x) < 0) -1 * delta_x else delta_x)
-      json_out$data$raster_collection_tiles[[bands]]$extent = tmp_extent
+      json_out$raster_collection_tiles[[bands]]$extent = tmp_extent
 
       times = as.numeric(dim(stars_obj)["time"])
       if(!is.na(times))
       {
         t_start = seq(from = attr(stars_obj[,,,bands,], "dimensions")$time$offset, by = attr(stars_obj[,,,bands,], "dimensions")$time$delta, length.out = times)
         t_end = c(t_start[2:length(t_start)], t_start[length(t_start)] + attr(stars_obj[,,,bands,], "dimensions")$time$delta)
-        json_out$data$raster_collection_tiles[[bands]]$start_times = as.list(as.character.POSIXt(t_start, format = "%Y-%m-%dT%T %Z"))
-        json_out$data$raster_collection_tiles[[bands]]$end_times = as.list(as.character.POSIXt(t_end, format = "%Y-%m-%dT%T %Z"))
+        json_out$raster_collection_tiles[[bands]]$start_times = as.list(as.character.POSIXt(t_start, format = "%Y-%m-%dT%T %Z"))
+        json_out$raster_collection_tiles[[bands]]$end_times = as.list(as.character.POSIXt(t_end, format = "%Y-%m-%dT%T %Z"))
       } else
       {
         t_start = NA
         t_end = NA
-        json_out$data$raster_collection_tiles[[bands]]$start_times = as.list(NA)
-        json_out$data$raster_collection_tiles[[bands]]$end_times = as.list(NA)
+        json_out$raster_collection_tiles[[bands]]$start_times = as.list(NA)
+        json_out$raster_collection_tiles[[bands]]$end_times = as.list(NA)
       }
       data = list()
       # if(!is.na(times)) length(data) = times else length(data) = 1
       if(is.na(times))
       {
+        print(Sys.time())
         cat("Time: 1...\n")
         bt_raster = as(stars_obj[,,,bands, drop = TRUE], "Raster")
         bt_df = as.data.frame(bt_raster, xy = TRUE)
@@ -207,7 +221,7 @@ stars2json = function(stars_obj, json_in)#, json_out_file = "udf_response.json")
       {
         for(t in 1:times)
         {
-          cat(paste("Time: ", t, "...\n", sep = ""))
+          cat(paste(Sys.time(), "; Time: ", t, "...\n", sep = ""))
           bt_raster = as(stars_obj[,,,bands,t, drop = TRUE], "Raster")
           bt_df = as.data.frame(bt_raster, xy = TRUE)
           uy = unique(bt_df[,2])
@@ -225,13 +239,14 @@ stars2json = function(stars_obj, json_in)#, json_out_file = "udf_response.json")
           data = c(data, list(y_list))
         }
       }
-      json_out$data$raster_collection_tiles[[bands]]$data = data
+      json_out$raster_collection_tiles[[bands]]$data = data
     }
   } else
   {
+    print(Sys.time())
     cat("Band: 1; ")
-    json_out$data$raster_collection_tiles = json_out$data$raster_collection_tiles[-c(2:length(json_out$data$raster_collection_tiles))]
-    tmp_extent = json_out$data$raster_collection_tiles[[1]][["extent"]]
+    json_out$raster_collection_tiles = json_out$raster_collection_tiles[-c(2:length(json_out$raster_collection_tiles))]
+    tmp_extent = json_out$raster_collection_tiles[[1]][["extent"]]
     if("time" %in% dimnames(stars_obj))
     {
       delta_x = attr(stars_obj[,,,], "dimensions")$x$delta
@@ -250,25 +265,26 @@ stars2json = function(stars_obj, json_in)#, json_out_file = "udf_response.json")
       y2 = attr(stars_obj[,,], "dimensions")$y$offset + delta_y * attr(stars_obj[,,], "dimensions")$y$to
     }
     tmp_extent = list(north = max(y1,y2), south = min(y1,y2), west = min(x1,x2), east = max(x1,x2), height = if(sign(delta_y) < 0) -1 * delta_y else delta_y, width = if(sign(delta_x) < 0) -1 * delta_x else delta_x)
-    json_out$data$raster_collection_tiles[[1]]$extent = tmp_extent
+    json_out$raster_collection_tiles[[1]]$extent = tmp_extent
 
     times = as.numeric(dim(stars_obj)["time"])
     if(!is.na(times))
     {
       t_start = seq(from = attr(stars_obj[,,,], "dimensions")$time$offset, by = attr(stars_obj[,,,], "dimensions")$time$delta, length.out = times)
       t_end = c(t_start[2:length(t_start)], t_start[length(t_start)] + attr(stars_obj[,,,], "dimensions")$time$delta)
-      json_out$data$raster_collection_tiles[[1]]$start_times = as.list(as.character.POSIXt(t_start, format = "%Y-%m-%dT%T %Z"))
-      json_out$data$raster_collection_tiles[[1]]$end_times = as.list(as.character.POSIXt(t_end, format = "%Y-%m-%dT%T %Z"))
+      json_out$raster_collection_tiles[[1]]$start_times = as.list(as.character.POSIXt(t_start, format = "%Y-%m-%dT%T %Z"))
+      json_out$raster_collection_tiles[[1]]$end_times = as.list(as.character.POSIXt(t_end, format = "%Y-%m-%dT%T %Z"))
     } else
     {
       t_start = NA
       t_end = NA
-      json_out$data$raster_collection_tiles[[1]]$start_times = as.list(NA)
-      json_out$data$raster_collection_tiles[[1]]$end_times = as.list(NA)
+      json_out$raster_collection_tiles[[1]]$start_times = as.list(NA)
+      json_out$raster_collection_tiles[[1]]$end_times = as.list(NA)
     }
     data = list()
     if(is.na(times))
     {
+      print(Sys.time())
       cat("Time: 1...\n")
       bt_raster = as(stars_obj[,,], "Raster")
       bt_df = as.data.frame(bt_raster, xy = TRUE)
@@ -289,7 +305,7 @@ stars2json = function(stars_obj, json_in)#, json_out_file = "udf_response.json")
     {
       for(t in 1:times)
       {
-        cat(paste("Time: ", t, "...\n", sep = ""))
+        cat(paste(Sys.time(), "; Time: ", t, "...\n", sep = ""))
         bt_raster = as(stars_obj[,,,t, drop = TRUE], "Raster")
         bt_df = as.data.frame(bt_raster, xy = TRUE)
         uy = unique(bt_df[,2])
@@ -307,13 +323,15 @@ stars2json = function(stars_obj, json_in)#, json_out_file = "udf_response.json")
         data = c(data, list(y_list))
       }
     }
-    json_out$data$raster_collection_tiles[[1]]$data = data
+    json_out$raster_collection_tiles[[1]]$data = data
   }
   # For writing to disk
   # write_json(x = json_out, path = json_out_file, auto_unbox = TRUE, pretty = TRUE)
-  json_response = toJSON(x = json_out, auto_unbox = TRUE, pretty = TRUE)
+  # json_response = toJSON(x = json_out, auto_unbox = TRUE, pretty = TRUE)
+  print(Sys.time())
   cat("Converted resulting stars object back to JSON!\n")
-  json_response
+  # json_response
+  json_out
 }
 
 json2fname = function(json_in)
@@ -322,22 +340,26 @@ json2fname = function(json_in)
 json2dim_mod = function(json_in)
   return(json_in$code$dim_mod)
 
+#' @serializer unboxedJSON
 #' @post /udf
 run_UDF.json = function(req)
 {
+  print(Sys.time())
   cat("Started executing at endpoint /udf\n")
   json_in = fromJSON(req$postBody, simplifyVector = FALSE)
   json2script(json_in)
 
   # udf_func = json2fname(json_in)
   # dim_mod = json2dim_mod(json_in)
-  udf_func = "median" #Testing
+  # udf_func = "median" #Testing
   dim_mod = 4         #Testing
 
   stars_in = json2stars(json_in)
-  stars_out = run_script(stars_obj = stars_in, dim_mod = dim_mod, function_name = "myfunc")
+  stars_out = run_script(stars_obj = stars_in, dim_mod = dim_mod)
   json_out = stars2json(stars_obj = stars_out, json_in = json_in)#, json_out_file = "udf_response.json")
 
   # Generate HTTP response for "backend" with body as the JSON in the file `json_out_file`
+  print(Sys.time())
+  cat("Generating response to HTTP request")
   json_out
 }
--- data/example_udf/median_json.R
+++ data/example_udf/median_json.R
@@ -6,31 +6,8 @@ library(openeo.R.UDF)
 
 cat("Defining median function\n")
 
-udf_func = function(obj) {
+function(obj) {
 
   median(obj)
 
 }
-
-
-
-cat("Running UDF function\n")
-
-# drop dimension is time = 4, input is default x,y,b,t,raster, function
-
-# tryCatch({
-#
-#   run_UDF(drop_dim = 4, function_name = myfunc,legend_name = "legend.csv")
-#
-# }, error = function(e) {
-#
-#   cat(paste("UDF-EXEC-ERROR:",e))
-#
-#   stop(e)
-#
-# })
-
-
-
-# cat("Finished running UDF function\n")
-
